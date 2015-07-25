1508ea298134fb3023bea9be7d258a8c9fc6c667
fr0wsTyl <frowstyl@gmail.com>
2015-07-25 20:10:51 +0300

    refactored the main JS file - script.js according to the best principles of HQC

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,100 +1,118 @@
 function game() {
+    //All global variables used in 'game' module
+    var stage,
+        gameBackground,
+        gameBackgroundImage,
+        shooter,
+        shooterImage,
+        ducks,
+        currentDuck,
+        currentDuckImage,
+        generatedRandom,
+        spawnXPosition,
+        spawnYPosition,
+        i,
+        CANVAS_WIDTH,
+        CANVAS_HEIGHT,
+        NUMBER_OF_DUCKS,
+        BIRDS_SIZE,
+        BIRDS_SPEED;
 
-	/* The old way of starting the game (via html button) */
-	// var initalLink = document.getElementById('initial');
 
-	// initalLink.style.display = 'none';
+    //Assigning default values for the constants
+    NUMBER_OF_DUCKS = 4;
+    BIRDS_SIZE = 75;
+    BIRDS_SPEED = 3;
+    CANVAS_WIDTH = 840;
+    CANVAS_HEIGHT = 620;
 
-	var stage = new Kinetic.Stage({
-		container: 'canvas-container',
-		width: 840,
-		height: 620
-	});
+    //Assigning default values for other variables
+    ducks = [];
+    generatedRandom = 0;
+    spawnXPosition = 0;
+    spawnYPosition = 0;
 
-	var layer = new Kinetic.Layer();
-	var backgroundLayer = new Kinetic.Layer();
+    //Initializing KineticJS Stage
+    stage = new Kinetic.Stage({
+        container: 'canvas-container',
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT
+    });
 
-	// background accepts width, height
-	var gameBackground = Object.create(background).init(840, 620),
-		gameBackgroundImage = gameBackground.draw();
+    layer = new Kinetic.Layer();
+    backgroundLayer = new Kinetic.Layer();
 
-	//xPosition, yPosition, width, height
-	var shooter = Object.create(shotgun).init(380, 525, 82, 97),
-		shooterImage = shooter.draw();
+    // background accepts width, height
+    gameBackground = Object.create(background).init(CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameBackgroundImage = gameBackground.draw();
 
-	var ducks = [],
-		numberOfDucks = 2,
-		birdsSize = 75,
-		speed = 3,
-		currentDuck,
-		currentDuckImage,
-		generatedRandom = 0,
-		spawnXPosition = 0,
-		spawnYPosition = 0;
+    //xPosition, yPosition, width, height
+    shooter = Object.create(shotgun).init(380, 525, 82, 97);
+    shooterImage = shooter.draw();
 
-	// xPosition yPosition, velocityX, velocityY, width, height, state
-	for (var i = 0; i < numberOfDucks; i += 1) {
-		generatedRandom = parseInt(Math.random() * 10);
-		spawnYPosition = (stage.getHeight() / 2) - generatedRandom * (birdsSize / 3);
+    // xPosition yPosition, velocityX, velocityY, width, height, state
+    for (i = 0; i < NUMBER_OF_DUCKS; i += 1) {
+        generatedRandom = parseInt(Math.random() * 10);
+        spawnYPosition = (stage.getHeight() / 2) - generatedRandom * (BIRDS_SIZE / 3);
 
-		if (generatedRandom % 2 === 0) {
-			spawnXPosition = 1;
-		} else {
-			spawnXPosition = stage.getWidth() - birdsSize - 10;
-		}
+        if (generatedRandom % 2 === 0) {
+            spawnXPosition = 1;
+        } else {
+            spawnXPosition = stage.getWidth() - BIRDS_SIZE - 10;
+        }
 
-		currentDuck = Object.create(duck).init(spawnXPosition, spawnYPosition, speed, speed, birdsSize, birdsSize, 'down-right'),
-			currentDuckImage = currentDuck.draw();
+        currentDuck = Object.create(duck).init(spawnXPosition, spawnYPosition, BIRDS_SPEED, BIRDS_SPEED, BIRDS_SIZE, BIRDS_SIZE, 'down-right');
+        currentDuckImage = currentDuck.draw();
 
-		ducks.push(currentDuck);
-		layer.add(currentDuckImage);
-	}
+        ducks.push(currentDuck);
+        layer.add(currentDuckImage);
+    }
 
-	backgroundLayer.add(gameBackgroundImage);
-	stage.add(backgroundLayer);
+    backgroundLayer.add(gameBackgroundImage);
+    stage.add(backgroundLayer);
 
-	layer.add(shooterImage);
-	stage.add(layer);
+    layer.add(shooterImage);
+    stage.add(layer);
 
-	function animFrame() {
-		ducks.forEach(function(ducky) {
-			var velocityX = ducky.velocityX,
-				velocityY = ducky.velocityY,
-				duckyImage = ducky.draw(),
-				currentX = duckyImage.getX() + velocityX,
-				currentY = duckyImage.getY() + velocityY,
-				suddenChangeDirectionX = parseInt(((Math.random() * 1000) + stage.getHeight() * currentY)),
-				suddenChangeDirectionY = parseInt(((Math.random() * 1000) + stage.getWidth() * currentX)),
-				magicNumber = 237;
+    function animFrame() {
+        ducks.forEach(function(ducky) {
+            var velocityX = ducky.velocityX,
+                velocityY = ducky.velocityY,
+                duckyImage = ducky.draw(),
+                currentX = duckyImage.getX() + velocityX,
+                currentY = duckyImage.getY() + velocityY,
+                suddenChangeDirectionX = parseInt(((Math.random() * 1000) + stage.getHeight() * currentY)),
+                suddenChangeDirectionY = parseInt(((Math.random() * 1000) + stage.getWidth() * currentX)),
+                magicNumber = 237;
 
-			ducky.clicked();
+            ducky.clicked();
 
-			if (0 >= currentX ||
-				currentX >= stage.getWidth() - birdsSize ||
-				suddenChangeDirectionX % magicNumber === 0) {
+            if (0 >= currentX ||
+                currentX >= stage.getWidth() - BIRDS_SIZE ||
+                suddenChangeDirectionX % magicNumber === 0) {
 
-				ducky.velocityX *= -1;
-				ducky.getRightDirection();
-			}
+                ducky.velocityX *= -1;
+                ducky.getRightDirection();
+            }
 
-			if (0 >= currentY ||
-				currentY >= stage.getHeight() - birdsSize ||
-				suddenChangeDirectionY % magicNumber === 0) {
+            if (0 >= currentY ||
+                currentY >= stage.getHeight() - BIRDS_SIZE ||
+                suddenChangeDirectionY % magicNumber === 0) {
 
-				ducky.velocityY *= -1;
-				ducky.getRightDirection();
-			}
+                ducky.velocityY *= -1;
+                ducky.getRightDirection();
+            }
 
-			duckyImage.setX(currentX);
-			duckyImage.setY(currentY);
-		});
+            duckyImage.setX(currentX);
+            duckyImage.setY(currentY);
+        });
 
-		shooter.clicked(stage);
+        shooter.clicked(stage);
 
-		layer.draw();
+        layer.draw();
 
-		requestAnimationFrame(animFrame);
-	}
+        requestAnimationFrame(animFrame);
+    }
 
-	animFrame();
+    animFrame();
 }
